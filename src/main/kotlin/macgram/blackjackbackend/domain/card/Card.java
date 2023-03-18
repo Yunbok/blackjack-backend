8f5c8b7d90919fb00272ce13a2e4855e8a71eb04
김범수 <beokim@gmarket.com>
2023-03-18 17:10:00 +0900

    feat: Card 클래스 추가, HoldCards 클래스 옮기기
    resolved: BLACKJACK-3

---
+++ src/main/kotlin/macgram/blackjackbackend/domain/card/Card.java
@@ -0,0 +1,44 @@
+package macgram.blackjackbackend.domain.card;
+
+import java.util.Objects;
+
+public class Card {
+    private Pattern pattern;
+    private Number number;
+
+    public Card(final Pattern pattern, final Number number) {
+        this.pattern = pattern;
+        this.number = number;
+    }
+
+    public boolean equalsAce() {
+        return Number.ACE.equals(this.number);
+    }
+
+    public Number getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Card card = (Card) o;
+        return pattern == card.pattern && number == card.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pattern, number);
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "pattern=" + pattern +
+                ", number=" + number +
+                '}';
+    }
+
+
+}

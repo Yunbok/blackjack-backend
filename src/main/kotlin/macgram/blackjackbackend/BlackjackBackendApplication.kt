80bdb3900b8ad08a468121eb756f3cdcd2878cb1
김범수 <beokim@gmarket.com>
2023-03-18 16:30:58 +0900

    spring project init

---
+++ src/main/kotlin/macgram/blackjackbackend/BlackjackBackendApplication.kt
@@ -0,0 +1,11 @@
+package macgram.blackjackbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlackjackBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<BlackjackBackendApplication>(*args)
+}

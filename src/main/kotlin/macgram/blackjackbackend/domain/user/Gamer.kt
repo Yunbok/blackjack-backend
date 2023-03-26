8ed6854f9321b49ef3bdb8e3d58631337503b88f
lcjltj <lcjltj@gmail.com>
2023-03-26 17:01:40 +0900

    feat: Gamer 인터페이스 Kotlin 변환
    resolved: BLACKJACK-15

---
+++ src/main/kotlin/macgram/blackjackbackend/domain/user/Gamer.kt
@@ -0,0 +1,12 @@
+package macgram.blackjackbackend.domain.user
+
+import macgram.blackjackbackend.domain.card.Card
+
+interface Gamer {
+    fun addCard(card: Card)
+    fun calculate(): Int
+    fun reset()
+}
+
+
+

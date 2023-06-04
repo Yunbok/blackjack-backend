74a51785b9bf49c8f1cb2d08a1667f5078699850
배윤복 <qodbsqhr@naver.com>
2023-06-04 17:08:40 +0900

    connect WebSocket resolved: BLACKJACK-22

--- src/main/kotlin/macgram/blackjackbackend/BlackjackBackendApplication.kt
+++ src/main/kotlin/macgram/blackjackbackend/BlackjackBackendApplication.kt
@@ -3,35 +3,36 @@ package macgram.blackjackbackend
 import macgram.blackjackbackend.domain.Game
 import macgram.blackjackbackend.domain.user.Player
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class BlackjackBackendApplication
 
 fun main(args: Array<String>) {
-//    runApplication<BlackjackBackendApplication>(*args)
+    runApplication<BlackjackBackendApplication>(*args)
 
-    val game = Game()
-    val player = Player(1000000L)
-    player.batting(10000L)
-    game.addPlayer(player)
-    game.start()
-
-    while (!game.isEnded()) {
-        game.players
-            .filter { it.continuable }
-            .forEach {
-                it.continuable = true
-            }
-        game.addPlayersCard()
-    }
-
-    game.continueDealerCard()
-    println("-----------------")
-
-    println("딜러: " + game.dealer.calculate())
-    println(game.dealer.hands)
-    game.players.forEach { println("유저: " + it.calculate()) }
-    game.players.forEach { println(it.hands) }
-    println("-----------------")
+//    val game = Game()
+//    val player = Player(1000000L)
+//    player.batting(10000L)
+//    game.addPlayer(player)
+//    game.start()
+//
+//    while (!game.isEnded()) {
+//        game.players
+//            .filter { it.continuable }
+//            .forEach {
+//                it.continuable = true
+//            }
+//        game.addPlayersCard()
+//    }
+//
+//    game.continueDealerCard()
+//    println("-----------------")
+//
+//    println("딜러: " + game.dealer.calculate())
+//    println(game.dealer.hands)
+//    game.players.forEach { println("유저: " + it.calculate()) }
+//    game.players.forEach { println(it.hands) }
+//    println("-----------------")
 
 }

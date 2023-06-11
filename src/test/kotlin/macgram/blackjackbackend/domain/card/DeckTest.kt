846486770bb7fc969ce47089e01d66d5901d6466
Yunbok <qodbsqhr@naver.com>
2023-06-11 17:48:36 +0900

    sss

--- src/test/kotlin/macgram/blackjackbackend/domain/card/DeckTest.kt
+++ src/test/kotlin/macgram/blackjackbackend/domain/card/DeckTest.kt
@@ -6,10 +6,5 @@ class DeckTest {
 
     @Test
     fun initDeck() {
-        val deck = Deck()
-
-        val deckSize = deck.deck.count()
-        println(deckSize)
-//        assert(deckSize == 56)
     }
 }
